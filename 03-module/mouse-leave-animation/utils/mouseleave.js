e2fdc9ea447f31619329c4217655deec828c6b9a
midCenturyDude7 <mgriffes@gmail.com>
2023-05-03 12:12:20 -0700

    Setting up modal to appear/disappear based on user behavior

--- 03-module/mouse-leave-animation/utils/mouseleave.js
+++ 03-module/mouse-leave-animation/utils/mouseleave.js
@@ -1,4 +1,15 @@
 /* eslint-disable */
 $(function () {
-  
+  $(window).mouseleave(function (e) {
+    const modalSeen = sessionStorage.getItem("modalSeen");
+    if (e.toElement == null && !modalSeen) {
+      document.documentElement.classList.add("mouse-out");
+    }
+  });
+
+  $("#close-modal").click(function (e) {
+    e.preventDefault();
+    document.documentElement.classList.remove("mouse-out");
+    sessionStorage.setItem("modalSeen", "true");
+  });
 });
